7d6c53df11ccd07085c342847b1a333405d1cad8
EthnoDev <dev@ethnocare.ca>
2026-07-27 16:48:32 -0400

    time for the options below the image

--- src/pages/sizing/liner/ThicknessSelection.jsx
+++ src/pages/sizing/liner/ThicknessSelection.jsx
@@ -2,8 +2,71 @@
 import { useTranslation } from "react-i18next";
 import PageWrapper from "../../../components/PageWrapper";
 
+// Asset Imports - Gel Cushion
+import gelCushion3Imperial from "../../../assets/thickness/Gel Cushion/3_imperial.svg";
+import gelCushion3Metric from "../../../assets/thickness/Gel Cushion/3_metric.svg";
+
+// Asset Imports - Gel Locking (Pin)
+import gelLocking3Imperial from "../../../assets/thickness/Gel Locking/3_imperial.svg";
+import gelLocking3Metric from "../../../assets/thickness/Gel Locking/3_metric.svg";
+
+// Asset Imports - TF Silicone Locking
+import tfSiliconeLockingImperial from "../../../assets/thickness/TF Silicone Locking/imperial.svg";
+import tfSiliconeLockingMetric from "../../../assets/thickness/TF Silicone Locking/metric.svg";
+
+// Asset Imports - TT Silicone Cushion
+import ttSiliconeCushion3Imperial from "../../../assets/thickness/TT Silicone Cushion/3_imperial.svg";
+import ttSiliconeCushion3Metric from "../../../assets/thickness/TT Silicone Cushion/3_metric.svg";
+
+// Asset Imports - TT Silicone Locking
+import ttSiliconeLocking3Imperial from "../../../assets/thickness/TT Silicone Locking/3_imperial.svg";
+import ttSiliconeLocking3Metric from "../../../assets/thickness/TT Silicone Locking/3_metric.svg";
+
 export default function ThicknessSelection() {
   const { t } = useTranslation(["pages", "common"]);
+
+  // 1. Get localStorage variables
+  const linerMaterial = localStorage.getItem("liner_material");
+  const suspension = localStorage.getItem("liner_suspension"); // 'cushion' or 'pin'
+  const isImperial = localStorage.getItem("units") === "imperial";
+  const amputation = localStorage.getItem("amputation"); // 'transfemoral' or 'transtibial'
+
+  const isSilicone = linerMaterial === "s30" || linerMaterial === "s40";
+  const isCushion = suspension === "cushion";
+
+  // 2. Resolve image based on decision tree
+  const getThicknessImage = () => {
+    if (!isSilicone) {
+      // GEL MATERIAL (Amputation doesn't matter)
+      if (isCushion) {
+        return isImperial ? gelCushion3Imperial : gelCushion3Metric;
+      } else {
+        // Locking / Pin
+        return isImperial ? gelLocking3Imperial : gelLocking3Metric;
+      }
+    } else {
+      // SILICONE MATERIAL (Amputation matters)
+      if (amputation === "transfemoral") {
+        return isImperial
+          ? tfSiliconeLockingImperial
+          : tfSiliconeLockingMetric;
+      } else {
+        // Transtibial
+        if (isCushion) {
+          return isImperial
+            ? ttSiliconeCushion3Imperial
+            : ttSiliconeCushion3Metric;
+        } else {
+          // Locking / Pin
+          return isImperial
+            ? ttSiliconeLocking3Imperial
+            : ttSiliconeLocking3Metric;
+        }
+      }
+    }
+  };
+
+  const selectedImage = getThicknessImage();
 
   return (
     <PageWrapper
@@ -23,7 +86,14 @@ export default function ThicknessSelection() {
           {t("thicknessLinerSizing.description")}
         </p>
 
-        {/* Dynamic content will be added here */}
+        {/* 3. Image Display */}
+        <div className="mt-8 flex justify-center">
+          <img
+            src={selectedImage}
+            alt={t("common:pages.liner_thickness")}
+            className="w-74 h-auto object-contain rounded-xl"
+          />
+        </div>
       </div>
     </PageWrapper>
   );
