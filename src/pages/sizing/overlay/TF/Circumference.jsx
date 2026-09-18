b07b55d4fa7a6f460bf903002c37f73ad0b1f574
EthnoDev <dev@ethnocare.ca>
2026-09-18 15:11:22 -0400

    TF bolding done

--- src/pages/sizing/overlay/TF/Circumference.jsx
+++ src/pages/sizing/overlay/TF/Circumference.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 import PageWrapper from "../../../../components/PageWrapper";
 import MeasurementInput from "../../../../components/MeasurementInput";
 
@@ -38,7 +38,16 @@ export default function Circumference() {
         </h1>
 
         <p className="mt-3 text-center text-base text-slate-500">
-          {t("circumferenceTFSizing.description", { ns: "pages", distance })}
+          <Trans
+            ns="pages"
+            i18nKey="circumferenceTFSizing.description"
+            values={{ distance }}
+            components={{
+              bold: <strong className="font-bold text-black" />,
+              underline: <span className="underline" />,
+              br: <br />
+            }}
+          />
         </p>
 
         <div className="mt-8 flex justify-center">
--- src/pages/sizing/overlay/TF/Length.jsx
+++ src/pages/sizing/overlay/TF/Length.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 import PageWrapper from "../../../../components/PageWrapper";
 import MeasurementInput from "../../../../components/MeasurementInput";
 
@@ -50,7 +50,15 @@ export default function Length() {
 
         {/* 2. Description */}
         <p className="mt-3 text-center text-base text-slate-500">
-          {t("lengthTFSizing.description", { ns: "pages" })}
+          <Trans
+            ns="pages"
+            i18nKey="lengthTFSizing.description"
+            components={{
+              bold: <strong className="font-bold text-black" />,
+              underline: <span className="underline" />,
+              br: <br />
+            }}
+          />
         </p>
 
         {/* 3. Image */}
